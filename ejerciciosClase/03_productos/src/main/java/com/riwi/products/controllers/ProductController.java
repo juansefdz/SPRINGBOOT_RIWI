fe281d44a04e57f7d21a084e90d3487abb9ac7d5
J. Sebastián Fernández M <137450466+juansefdz@users.noreply.github.com>
2024-04-23 15:43:02 -0500

    Add files via upload

---
+++ ejerciciosClase/03_productos/src/main/java/com/riwi/products/controllers/ProductController.java
@@ -0,0 +1,64 @@
+package com.riwi.products.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.riwi.products.entities.Product;
+import com.riwi.products.services.service_abstract.IProductService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@AllArgsConstructor
+public class ProductController {
+    @Autowired
+    private final IProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll() {
+        return ResponseEntity.ok(this.productService.getAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Product> get(@PathVariable Long id) {
+        return ResponseEntity.ok(this.productService.findById(id));
+    }
+
+    @GetMapping(path = "/search")
+    public ResponseEntity<List<Product>> get(@RequestParam String name) {
+        return ResponseEntity.ok(this.productService.search(name));
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> insert(@RequestBody Product objProduct) {
+        return ResponseEntity.ok(this.productService.save(objProduct));
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Product> update(
+            @PathVariable Long id, /** PathVariable porque el id viene por url */
+            @RequestBody Product product) /** RequestBody porque los datos vienen por el cuerpo de la petición */
+    {
+        return ResponseEntity.ok(this.productService.update(id, product));
+
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
